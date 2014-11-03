a654a31d470f807c8f793adaa6f2a06562ed1bdf
Meagan Waller <meaganewaller@gmail.com>
2014-11-03 18:59:46 -0500

    adds flash tytpes

--- helpers.rb
+++ helpers.rb
@@ -4,4 +4,8 @@ module Helpers
   def partial(page, options={})
     erb "_#{page}".to_sym, options.merge!(:layout => false)
   end
+
+  def flash_types
+    [:success, :notice, :warning, :error]
+  end
 end
